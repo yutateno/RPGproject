76b205dc92074119f72d751277209d5147091207
nakagawa <kingwasheart@yahoo.co.jp>
2017-02-07 16:54:09 +0900

    Dungeon作成、マップ生成のみ、プレイヤー位置を中心にする画面範囲変更に関してはコメントアウトにて

--- RPGproject/RPGproject/Dungeon.cpp
+++ RPGproject/RPGproject/Dungeon.cpp
@@ -11,6 +11,9 @@ Dungeon::Dungeon() {
 	Gr_Back = LoadGraph("Dungeon\\Dungeon_Back.png");
 	Gr_Wall = LoadGraph("Dungeon\\wall.png");
 	Gr_Player = LoadGraph("Dungeon\\player.png");
+
+	player_x = 0;
+	player_y = 0;
 }
 
 Dungeon::~Dungeon() {
@@ -32,6 +35,7 @@ void Dungeon::UpDate() {
 		endFlag = true;	// エラー終了
 		break;
 	}
+	MapDate();
 }
 void Dungeon::UpDate_Start() {
 	startCount++;
@@ -82,22 +86,79 @@ void Dungeon::Draw() {
 }
 
 void Dungeon::Draw_Start() {
-	SetDrawBlendMode(DX_BLENDMODE_ALPHA, startCount * 1);
-	DrawRotaGraph(320, 240, 1.0, startCount * 1, Gr_Back, FALSE);	//回転角度 1  遊んでる"だけ"
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, startCount * 3);
+	DrawGraph(0, 0, Gr_Back, FALSE);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 	//DrawStringToHandle(0, 0, "ダンジョン画面", WHITE, Font::Get(eFont::SELECT));
 	//DrawFormatStringToHandle(0, 100, WHITE, Font::Get(eFont::SELECT), "開始画面%d", this->startCount);
 }
 
 void Dungeon::Draw_Main() {
-	DrawStringToHandle(0, 0, "ダンジョン画面", WHITE, Font::Get(eFont::SELECT));
-	DrawStringToHandle(0, 100, "メイン処理画面", WHITE, Font::Get(eFont::SELECT));
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 150);
+	DrawGraph(0, 0, Gr_Back, FALSE);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+	for (int i = 0, n = (int)map.size(); i < n; i++) {
+		for (int j = 0, m = (int)map[i].size(); j < m; j++) {
+			// 画面内なら描画　playerの位置を左上として右下までの範囲です
+			if (j * 28 + player_x >= -28 && j * 28 + player_x <= 640 &&
+				i * 28 + player_y >= -28 && i * 28 + player_y <= 480) {
+				//stoi で文字を数値に変換
+				switch ((int)(stoi(map[i][j]) * 0.1)) {
+				case 0:	//00
+					break;
+
+				case 1:	//壁
+					switch (stoi(map[i][j]) % 10) {
+					case 0:	//10
+						DrawGraph(j * 28 + player_x, i * 28 + player_y, Gr_Wall, false);
+						break;
+
+					default:
+						break;
+					}
+					break;
+				case 3:
+					switch (stoi(map[i][j]) % 10) {
+					case 0:
+						DrawGraph(j * 28 + player_x, i * 28 + player_y, Gr_Player, false);
+						break;
+
+					default:
+						break;
+					}
+
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+
+	//DrawStringToHandle(0, 0, "ダンジョン画面", WHITE, Font::Get(eFont::SELECT));
+	//DrawStringToHandle(0, 100, "メイン処理画面", WHITE, Font::Get(eFont::SELECT));
 	DrawStringToHandle(0, 200, "Zキーで戦闘画面へ", WHITE, Font::Get(eFont::SELECT));
 	DrawStringToHandle(0, 300, "Xキーでフィールド画面へ", WHITE, Font::Get(eFont::SELECT));
 	DrawStringToHandle(0, 400, "Cキーでゲームクリア画面へ", WHITE, Font::Get(eFont::SELECT));
 }
 
 void Dungeon::Draw_End() {
-	DrawStringToHandle(0, 0, "ダンジョン画面", WHITE, Font::Get(eFont::SELECT));
-	DrawFormatStringToHandle(0, 100, WHITE, Font::Get(eFont::SELECT), "終了画面%d", this->endCount);
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, 150 - (startCount * 1));
+	DrawRotaGraph(320, 240, 1.0, startCount * 1, Gr_Back, FALSE);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	//DrawStringToHandle(0, 0, "ダンジョン画面", WHITE, Font::Get(eFont::SELECT));
+	//DrawFormatStringToHandle(0, 100, WHITE, Font::Get(eFont::SELECT), "終了画面%d", this->endCount);
+}
+
+void Dungeon::MapDate() {
+	read_file.open("Dungeon\\DungeonMap.txt");
+	read_count = 0;
+	while (getline(read_file, read_line)) {	// 一行ずつ読み込み
+		map.resize(read_count + 1);
+		for (int i = 0, n = (int)read_line.length(); i < n; i += 2) {
+			map[read_count].push_back(read_line.substr(i, 2));
+		}
+		read_count++;	// 次の行に
+	}
 }
--- RPGproject/RPGproject/Dungeon.h
+++ RPGproject/RPGproject/Dungeon.h
@@ -3,14 +3,22 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <deque>
+#include <vector>
 
 // ダンジョン
 class Dungeon : public SuperScene {
 private:
 	int Gr_Back;	// 背景
-	int Gr_Player;	//プレイヤー
-	int Gr_Wall;	//キャラ
+	int Gr_Player;
+	int Gr_Wall;
+
+	vector<vector<string>> map;
+
+	ifstream read_file;		// 読み込むファイル
+	string read_line;		// 読み込んだ行（1行）
+	int read_count;
+	int player_x;
+	int player_y;
 
 public:
 	Dungeon();
@@ -25,4 +33,6 @@ public:
 	void Draw_Start();	// 開始画面描画
 	void Draw_Main();	// メイン画面描画
 	void Draw_End();	// 終了画面描画
+
+	void MapDate();
 };
